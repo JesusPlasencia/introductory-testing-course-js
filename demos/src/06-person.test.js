32953baa21db1007665af83a7807e7ebd9449496
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-12-20 10:21:03 -0500

    feat: coverage person full

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -6,6 +6,18 @@ describe('Test for Person', () => {
     person = new Person('Jesus', 45, 1.7);
   });
 
+  test('should return not found', () => {
+    person.weight = undefined;
+    const imc = person.calcIMC();
+    expect(imc).toBe('not found');
+  });
+
+  test('should return no found', () => {
+    person.weight = -5;
+    const imc = person.calcIMC();
+    expect(imc).toBe('no found');
+  });
+
   test('should return down', () => {
     //! AAA
     //! Arrange / Given
@@ -20,5 +32,29 @@ describe('Test for Person', () => {
     person.weight = 59;
     const imc = person.calcIMC();
     expect(imc).toBe('normal');
+  });
+
+  test('should return overweight', () => {
+    person.weight = 75;
+    const imc = person.calcIMC();
+    expect(imc).toBe('overweight');
+  });
+
+  test('should return overweight level 1', () => {
+    person.weight = 80;
+    const imc = person.calcIMC();
+    expect(imc).toBe('overweight level 1');
+  });
+
+  test('should return overweight level 2', () => {
+    person.weight = 90;
+    const imc = person.calcIMC();
+    expect(imc).toBe('overweight level 2');
+  });
+
+  test('should return overweight level 3', () => {
+    person.weight = 120;
+    const imc = person.calcIMC();
+    expect(imc).toBe('overweight level 3');
   });
 });
