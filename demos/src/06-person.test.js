55f91025aebfbad99d9f7f5c0ce9ced567d88bc1
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-12-20 09:51:43 -0500

    fix: comment

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -7,8 +7,12 @@ describe('Test for Person', () => {
   });
 
   test('should return down', () => {
+    //! AAA
+    //! Arrange / Given
     person.weight = 45;
+    //! Act / When
     const imc = person.calcIMC();
+    //! Assert / Then
     expect(imc).toBe('down');
   });
 
