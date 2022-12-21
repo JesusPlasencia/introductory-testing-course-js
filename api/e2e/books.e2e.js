90e150fd7fa902babe548dd953ca8816594a4780
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-12-21 12:50:41 -0500

    feat: e2e

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,39 +1,57 @@
-const mockGetAll = jest.fn();
 const request = require('supertest');
-
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
-const { generateManyBook } = require('../src/fakes/book.fake');
+const { config } = require('../src/config/index');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => { },
-})));
+// !VARIABLES DB
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
+const COLLECTION = 'books';
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   describe('Test for [GET] /api/v1/books', () => {
-    test('should return books', () => {
+    test('should return books', async () => {
       // Arrange
-      const fakeBooks = generateManyBook(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
-      // Act
+      const seedData = await database.collection(COLLECTION).insertMany([
+        {
+          name: 'Book 1',
+          year: 1998,
+          author: 'JP',
+        },
+        {
+          name: 'Book 2',
+          year: 2022,
+          author: 'JP',
+        },
+      ]);
+      console.log(seedData);
       return request(app)
         .get('/api/v1/books')
         .expect(200)
         .then(({ body }) => {
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          console.log(body);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
-  });
 
-  afterAll(async () => {
-    await server.close();
+    afterAll(async () => {
+      await server.close();
+      await database.dropDatabase();
+    });
   });
 });
